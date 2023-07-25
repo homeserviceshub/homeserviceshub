ab263dd0a47a8f9e46e62ca595136438b9c2aa44
homeserviceshub <homeserviceshub1@gmail.com>
2023-07-26 01:38:17 +0530

    edited footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,42 +1,71 @@
 import React from "react";
 import styles from "./index.module.css";
 import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import IconFacebook from "../icons/IconFacebook";
+import IconInstagram from "../icons/IconInstagram";
+import IconLinkedIn from "../icons/IconLinkedIn";
+import IconYoutube from "../icons/IconYoutube";
+import IconTwitter from "../icons/IconTwitter";
 
 const Footer = () => {
+  const socialMedia = [
+    { link: "https://www.facebook.com/", icon: <IconFacebook /> },
+    { link: "https://www.instagram.com/", icon: <IconInstagram /> },
+    { link: "https://www.linkedin.com/", icon: <IconLinkedIn /> },
+    { link: "https://www.youtube.com/", icon: <IconYoutube /> },
+    { link: "https://www.twitter.com/", icon: <IconTwitter /> },
+  ];
   return (
     <div className={styles.footerArea}>
       <Container>
         <Row>
           <Col lg={3} md={6} sm={6}>
             <div>
-              <h6 className={styles.footerTitle}>About</h6>
-              <p className={styles.footerPara}>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore dolore magna aliqua.
-              </p>
+              <h6 className={styles.footerTitle}>Company</h6>
+              <Link to={"/aboutus"} className={styles.link}>
+                About Us
+              </Link>
+              <Link to={"/contactus"} className={styles.link}>
+                Contact Us
+              </Link>
+              <Link to={"/faq"} className={styles.link}>
+                FAQ's
+              </Link>
+              <Link to={"/careers"} className={styles.link}>
+                Careers
+              </Link>
+              <Link to={"/aboutus"} className={styles.link}>
+                Help
+              </Link>
+              <Link to={"/aboutus"} className={styles.link}>
+                Terms & Conditions
+              </Link>
             </div>
           </Col>
           <Col lg={3} md={6} sm={6}>
             <div>
-              <h6 className={styles.footerTitle}>Contact Us</h6>
-              <p className={styles.footerPara}>
-                you can connect with us through email and also ontact us by
-                provided numbers below
-              </p>
+              <h6 className={styles.footerTitle}>Be an Ace</h6>
+              <Link to={"/ace"} className={styles.link}>
+                Join Us
+              </Link>
+              <Link to={"/ace/signin"} className={styles.link}>
+                Ace Login
+              </Link>
+              <Link to={"/ace/terms"} className={styles.link}>
+                Terms & Conditions
+              </Link>
             </div>
-            <a href="tel:8054875055" className={styles.numbers}>
-              80548-75055
-            </a>
-            <a href="tel:8054875055" className={styles.numbers}>
-              80548-75055
-            </a>
           </Col>
           <Col lg={5} md={6} sm={6}>
             <div>
               <h6 className={styles.footerTitle}>Newsletter</h6>
               <p className={styles.footerPara}>
-                You can trues use we eill never send you promo offers, not a
-                single spam
+                {/* To get the latest news and offers join our newsletter and you
+                can trust us we will never send you promo offers, not a single
+                spam */}
+                Unlock Exclusive Offers! Trust Us, No Annoying Promos, Just Pure
+                Value!
               </p>
               <div>
                 <Form>
@@ -55,11 +84,20 @@ const Footer = () => {
           <Col lg={8} md={8} m={0} className={styles.footerPara}>
             Copyright © 2023 All rights reserved
           </Col>
-          <Col lg={4} md={4}>
-            {/* <a href="#">Facebook</a>
-            <a href="#">Twiter</a>
-            <a href="#">LinkedIn</a>
-            <a href="#">Instagram</a> */}
+          <Col lg={4} md={4} className={styles.iconDiv}>
+            {socialMedia.map((item, index) => {
+              return (
+                <div key={index} className={styles.icon}>
+                  <Link
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    to={item.link}
+                  >
+                    {item.icon}
+                  </Link>
+                </div>
+              );
+            })}
           </Col>
         </Row>
       </Container>
